492120762627b2ae0aad8f53990caa988c52d2f2
chucoding <chucoding@gmail.com>
2023-10-15 19:57:37 +0900

    fix : PUSH 이벤트 리스너 수정(ncloud API에 맞게)

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,32 +1,23 @@
 self.addEventListener("install", function (e) {
-    console.log("fcm sw install..");
-    self.skipWaiting();
-  });
+  console.log("fcm sw install..");
+  self.skipWaiting();
+});
   
-  self.addEventListener("activate", function (e) {
-    console.log("fcm sw activate..");
-  });
+self.addEventListener("activate", function (e) {
+  console.log("fcm sw activate..");
+});
   
-  self.addEventListener("push", function (e) {
-    console.log("push: ", e.data.json());
-    if (!e.data.json()) return;
-  
-    const resultData = e.data.json().notification;
-    const notificationTitle = resultData?.content;
-    const notificationOptions = {
-      body: resultData?.body,
-      icon: resultData?.image,
-      tag: resultData?.tag,
-      ...resultData,
-    };
-    console.log("push: ", { resultData, notificationTitle, notificationOptions });
-  
-    self.registration.showNotification(notificationTitle, notificationOptions);
-  });
-  
-  self.addEventListener("notificationclick", function (event) {
-    console.log("notification click");
-    const url = "/";
-    event.notification.close();
-    event.waitUntil(clients.openWindow(url));
-  });
+self.addEventListener("push", function (e) {
+  if (!e.data.json()) return;
+  const result = e.data.json();
+  const notificationTitle = result.data.content;
+  console.log(notificationTitle)
+  self.registration.showNotification(notificationTitle); // alert 제목을 보여줌
+});
+
+self.addEventListener("notificationclick", function (event) {
+  console.log("notification click");
+  const url = "/";
+  event.notification.close();
+  event.waitUntil(clients.openWindow(url));
+});
