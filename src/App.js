a66254fa1a4ea943415ec742ac59819094fd86e9
chucoding <chucoding@gmail.com>
2023-10-20 23:27:41 +0900

    feat : markdown viewer

--- src/App.js
+++ src/App.js
@@ -2,7 +2,13 @@ import React, { useState, useEffect } from 'react';
 import { initializeApp } from "firebase/app";
 import { getMessaging, getToken, onMessage } from "firebase/messaging";
 import ReactMarkdown from 'react-markdown';
+import remarkGfm from 'remark-gfm'
+//import { Light as SyntaxHighlighter } from 'react-syntax-highlighter';
+//import { docco } from 'react-syntax-highlighter/dist/esm/styles/hljs';
+import {Prism as SyntaxHighlighter} from 'react-syntax-highlighter'
+import {dark} from 'react-syntax-highlighter/dist/esm/styles/prism'
 import Annotation from './modules/Annotation';
+import "../src/markdown.css";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
@@ -45,15 +51,46 @@ const ONE_WEEK_AGO = 7;
 const ONE_MONTH_AGO = 30;
 
 function App() {
+  //TODO API 호출할때마다 키 값 저장해야함. 안 그러면 다음 API 호출시 중복 발생
+  const [markdownText, setMarkdownText] = useState({1:``,7:``,30:``});
+  const getData = async (day) => {
+    const commits = await getCommits(day);
+    for (let commit of commits) {
+      const commit_detail = await getFilename(commit.sha);
+      for (let file of commit_detail.files) {
+          if(file.filename.endsWith(".md")) {
+            const markdown = await getMarkdown(file.filename); //TODO markdown 다 받을때까지 대기한다음 setState 할 수 있도록
+            console.log(markdown);
+            markdownText[day] = markdown;
+            setMarkdownText(markdownText);
+          }
+      }
+    }
+  }
 
-  const [contents, setContents] = useState({ONE_DAY_AGO:{}, ONE_WEEK_AGO:{}, ONE_MONTH_AGO:{}});
+  const getCommits = async (day) => {
+    return await fetch("https://api.github.com/repos/chucoding/today-i-learned/commits?since=2023-09-05&until=2023-09-06", {
+      headers : {
+        "Authorization":`Bearer ${process.env.REACT_APP_GITHUB_TOKEN}`
+      }
+    }).then(res=>res.json());
+  }
 
-  const getData = (day) => {
-    fetch("")
-    .then(res=>res.json())
-    .then(data => {
-      setContents(...contents, {day:data})
-    })
+  const getFilename = async (commit_sha) => {
+    return await fetch(`https://api.github.com/repos/chucoding/today-i-learned/commits/${commit_sha}`, {
+      headers : {
+        "Authorization":`Bearer ${process.env.REACT_APP_GITHUB_TOKEN}`
+      }
+    }).then(res=>res.json());
+  }
+
+  const getMarkdown = async (filename) => {
+    return await fetch(`https://api.github.com/repos/chucoding/today-i-learned/contents/${filename}`, {
+      headers : {
+        "Accept":"application/vnd.github.raw",
+        "Authorization":`Bearer ${process.env.REACT_APP_GITHUB_TOKEN}`
+      }
+    }).then(response =>response.text());
   }
 
   useEffect(()=>{
@@ -65,17 +102,77 @@ function App() {
   return (
     <div>
       <Annotation text="/* Day1 */" />
-      <ReactMarkdown>
-      {contents[ONE_DAY_AGO]}
-      </ReactMarkdown>
+      <ReactMarkdown
+        remarkPlugins={[remarkGfm]}
+        components={{
+          code: ({ node, inline, className, children, ...props }) => {
+            const match = /language-(\w+)/.exec(className || '');
+            return !inline && match ? (
+              <SyntaxHighlighter
+                style={dark}
+                language={match[1]}
+                PreTag="div"
+                children={String(children).replace(/\n$/, '')}
+                {...props}
+              />
+            ) : (
+              <code className={className} {...props}>
+                {children}
+              </code>
+            );
+          },
+        }}
+        children={markdownText[ONE_DAY_AGO]}
+        className="markdown-table"
+      />
       <Annotation text="/* Day7 */" />
-      <ReactMarkdown>
-      {contents[ONE_WEEK_AGO]}
-      </ReactMarkdown>
+      <ReactMarkdown
+        remarkPlugins={[remarkGfm]}
+        components={{
+          code: ({ node, inline, className, children, ...props }) => {
+            const match = /language-(\w+)/.exec(className || '');
+            return !inline && match ? (
+              <SyntaxHighlighter
+                style={dark}
+                language={match[1]}
+                PreTag="div"
+                children={String(children).replace(/\n$/, '')}
+                {...props}
+              />
+            ) : (
+              <code className={className} {...props}>
+                {children}
+              </code>
+            );
+          },
+        }}
+        children={markdownText[ONE_WEEK_AGO]}
+        className="markdown-table"
+      />
       <Annotation text="/* Day30 */" />
-      <ReactMarkdown>
-      {contents[ONE_MONTH_AGO]}
-      </ReactMarkdown>
+      <ReactMarkdown
+        remarkPlugins={[remarkGfm]}
+        components={{
+          code: ({ node, inline, className, children, ...props }) => {
+            const match = /language-(\w+)/.exec(className || '');
+            return !inline && match ? (
+              <SyntaxHighlighter
+                style={dark}
+                language={match[1]}
+                PreTag="div"
+                children={String(children).replace(/\n$/, '')}
+                {...props}
+              />
+            ) : (
+              <code className={className} {...props}>
+                {children}
+              </code>
+            );
+          },
+        }}
+        children={markdownText[ONE_MONTH_AGO]}
+        className="markdown-table"
+      />
     </div>
   );
 }
--- src/modules/Annotation.js
+++ src/modules/Annotation.js
@@ -1,5 +1,4 @@
 export default function Annotation({text}) {
-    console.log(text);
     return (
         <i style={{color:'lightgray'}}>{text}</i>
     );
